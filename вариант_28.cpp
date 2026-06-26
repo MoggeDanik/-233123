2ee9824e8c50e85778d98884963a9406847008d6
MoggeDanik <golovejdaniil@gmail.com>
2026-06-26 17:40:01 +0300

    Update вариант_28.cpp

--- вариант_28.cpp
+++ вариант_28.cpp
@@ -39,4 +39,38 @@ int main() {
         std::cout << "Введите название школы: ";
         std::getline(std::cin, child.schoolName);
         std::cout << "Введите класс: ";
-        std::cin >> child.g
+        std::cin >> child.grade;
+        
+        std::cout << "\nИнформация о школьнике:" << std::endl;
+        std::cout << "ФИО: " << child.person.fullName << std::endl;
+        std::cout << "Дата рождения: " << child.person.birthDate.day << "/" 
+                  << child.person.birthDate.month << "/" << child.person.birthDate.year << std::endl;
+        std::cout << "Школа: " << child.schoolName << std::endl;
+        std::cout << "Класс: " << child.grade << std::endl;
+    } else if (type == 2) {
+        Student student;
+        std::cout << "Введите ФИО: ";
+        std::getline(std::cin, student.person.fullName);
+        std::cout << "Введите дату рождения (день месяц год): ";
+        std::cin >> student.person.birthDate.day >> student.person.birthDate.month >> student.person.birthDate.year;
+        std::cin.ignore();
+        std::cout << "Введите название вуза: ";
+        std::getline(std::cin, student.university);
+        std::cout << "Введите факультет: ";
+        std::getline(std::cin, student.faculty);
+        std::cout << "Введите название группы: ";
+        std::getline(std::cin, student.groupName);
+        
+        std::cout << "\nИнформация о студенте:" << std::endl;
+        std::cout << "ФИО: " << student.person.fullName << std::endl;
+        std::cout << "Дата рождения: " << student.person.birthDate.day << "/" 
+                  << student.person.birthDate.month << "/" << student.person.birthDate.year << std::endl;
+        std::cout << "Вуз: " << student.university << std::endl;
+        std::cout << "Факультет: " << student.faculty << std::endl;
+        std::cout << "Группа: " << student.groupName << std::endl;
+    } else {
+        std::cout << "Неверный выбор" << std::endl;
+    }
+    
+    return 0;
+}
